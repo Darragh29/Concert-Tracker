0f701c88e1e5849b11a94565751d6482d7e4ef7a
Darragh Doherty <darraghjdoherty29@gmail.com>
2026-05-06 01:40:34 +0100

    fix: Changed display 'name' to 'artist' and added env example

--- src/display.py
+++ src/display.py
@@ -6,4 +6,4 @@ def display_concert(concerts):
     for i,concert in enumerate(concerts,1):
         format_date = datetime.strptime(concert['date'],'%Y-%m-%d')
         date = format_date.strftime('%d %B %Y')
-        print(f"{i}.\nName: {concert['name']}\nCity: {concert['city']}\nVenue: {concert['venue']}\nDate: {date}\nBuy Tickets Here: {concert['link']}\n\n")
+        print(f"{i}.\nArtist: {concert['name']}\nCity: {concert['city']}\nVenue: {concert['venue']}\nDate: {date}\nBuy Tickets Here: {concert['link']}\n\n")
